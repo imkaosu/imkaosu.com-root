e17704c897daa23ca75f4f94e29630b36ac7ca79
k40su <kaosu243@gmail.com>
2025-12-20 22:47:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const homeBtn = document.getElementById('homeBtn');
+  if (homeBtn) {
+    homeBtn.addEventListener('click', () => {
+      window.location.href = 'index.html';
+    });
+  }
+});
